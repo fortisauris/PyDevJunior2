ef6d8ae512f455c350115f48850dfffb85ffea85
fortisauris <fortisauris@gmail.com>
2023-04-26 18:11:49 +0200

    session16 pridana

--- session16.py
+++ session16.py
@@ -3,8 +3,14 @@
 
 # jednoduchy generator mesiacov
 
+def generator_mesiacov():
+    mesiace = ('jan', 'feb','mar','apr')  # tuple
+    for i in mesiace:
+        yield i
 
-
+mes = generator_mesiacov()
+for i in range(15):
+    print(next(mes))
 
 # januar, februar, marec ....
 
